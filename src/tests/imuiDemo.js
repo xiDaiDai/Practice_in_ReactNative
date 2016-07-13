0a2c495fd408c867dd863b66de36e4ff92e43945
xiDaiDai <xishiwei@sina.cn>
2016-07-13 15:11:09 +0800

    QAQ

--- src/tests/imuiDemo.js
+++ src/tests/imuiDemo.js
@@ -15,30 +15,37 @@ import {
 
 
 var datas = [{
+  type: 'pic',
   isMe: false,
-  talkContent: '最近在学习React Native！',
+  image: 'http://ww2.sinaimg.cn/large/610dc034jw1f5s5382uokj20fk0ncmyt.jpg'
 }, {
+  type: 'text',
   isMe: true,
-  talkContent: '听说是个跨平台开发原生App的开源引擎',
+  talkContent: 'react native是什么',
 }, {
+  type: 'text',
   isMe: false,
-  talkContent: '感觉编不下去对话了呀.......感觉编不下去对话了呀......感觉编不下去对话了呀......',
+  talkContent: '欢迎来到侏罗纪公园，里面有各种啤酒，烤串，鱼雷世界',
 }, {
+  type: 'pic',
   isMe: true,
-  talkContent: '无语！',
+  image: 'http://ww2.sinaimg.cn/large/610dc034jw1f5s5382uokj20fk0ncmyt.jpg'
 }, {
+  type: 'text',
   isMe: false,
-  talkContent: '自说自话，好难！随便补充点字数吧，嗯 就酱紫 :) ',
+  talkContent: '顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶的顶顶顶顶顶顶顶顶顶顶',
 }, {
+  type: 'text',
   isMe: true,
-  talkContent: '感觉编不下去对话了呀......感觉编不下去对话了呀..',
+  talkContent: '快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快快',
 }, {
+  type: 'pic',
   isMe: false,
-  talkContent: 'GG,思密达编不下去了！',
+  image: 'http://gold.xitu.io/favicons/apple-touch-icon-180x180.png'
 }, ];
 
 
-export default class FarmChildView extends React.Component {
+export default class IMUI extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -60,19 +67,22 @@ export default class FarmChildView extends React.Component {
     return (
       <View style={eData.isMe==false?styles.everyRowRight:styles.everyRow}>
           <Image
-            source={eData.isMe==false? null:require('../images/mine.png')}
-            style={eData.isMe==false?null:styles.talkImg}
+            source={eData.isMe==false? null:{uri:'http://avatars1.githubusercontent.com/u/9985454?v=3&u=2d366f81a3ff39f4439e7cec24ab9fa0cdb48b22&s=140'}}
+            style={eData.isMe==false?null:styles.avatarLeft}
           />
           <View style={{flex:1}}>
-          <View style={eData.isMe==false?styles.talkViewRight:styles.talkView}>
-            <Text style={eData.isMe==false?styles.talkTextRight:styles.talkText  }>
+          <View style={eData.isMe==false?styles.talkViewRight:styles.talkViewLeft}>
+            {eData.type!='pic'?
+            (<Text style={eData.isMe==false?styles.talkTextRight:styles.talkTextLeft}>
                   {eData.talkContent}
-            </Text>
+            </Text>):(<Image
+            source={{uri:eData.image}}
+            style={styles.contentImg}/>)}
           </View>
           </View>
           <Image
-            source={eData.isMe==false? require('../images/mine.png') :null}
-            style={eData.isMe==false?styles.talkImgRight:null}
+            source={eData.isMe==false? {uri:'http://tp2.sinaimg.cn/1831355877/50/5651946296/1'} :null}
+            style={eData.isMe==false?styles.avatarRight:null}
           />
         </View>
     );
@@ -133,7 +143,7 @@ export default class FarmChildView extends React.Component {
                   <TextInput
                       ref='_textInput'
                       onChangeText={(text) =>{this.state.inputContentText=text}}
-                      placeholder=' 请输入对话内容'
+                      placeholder=' 请输入内容'
                       returnKeyType='done'
                       style={styles.inputText}
                   />
@@ -173,11 +183,12 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#DDDDDD',
     height: 52,
-    padding: 5
+    padding: 5,
+    marginBottom: 10
   },
   sendBtn: {
     alignItems: 'center',
-    backgroundColor: '#FF88C2',
+    backgroundColor: '#fff',
     padding: 10,
     borderRadius: 5,
     height: 40,
@@ -187,9 +198,6 @@ var styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
-
-
-
   searchBox: {
     height: 40,
     flexDirection: 'row',
@@ -217,10 +225,10 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-end'
   },
-  talkView: {
+  talkViewLeft: {
     flex: 1,
     backgroundColor: 'white',
-    padding: 10,
+    padding: 5,
     borderRadius: 5,
     marginLeft: 5,
     marginRight: 55,
@@ -230,14 +238,14 @@ var styles = StyleSheet.create({
   talkViewRight: {
     flex: 1,
     backgroundColor: '#90EE90',
-    padding: 10,
+    padding: 5,
     borderRadius: 5,
     marginLeft: 55,
     marginRight: 5,
     marginBottom: 10,
     alignSelf: 'flex-end',
   },
-  talkText: {
+  talkTextLeft: {
     fontSize: 16,
     fontWeight: 'bold',
   },
@@ -246,16 +254,23 @@ var styles = StyleSheet.create({
     fontWeight: 'bold',
     alignSelf: 'flex-end',
   },
-  talkImg: {
-    height: 40,
-    width: 40,
+  avatarLeft: {
+    height: 50,
+    width: 50,
     marginLeft: 10,
-    marginBottom: 10
+    marginBottom: 10,
+    borderRadius: 25
   },
-  talkImgRight: {
-    height: 40,
-    width: 40,
+  avatarRight: {
+    height: 50,
+    width: 50,
     marginRight: 10,
-    marginBottom: 10
+    marginBottom: 10,
+    borderRadius: 25
+  },
+
+  contentImg: {
+    height: 100,
+    width: 80,
   },
 });
